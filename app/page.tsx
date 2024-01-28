96286aa25f7a6bc1f6cebdedd7c6ece4b2337b3e
Bibek Ghimire <89078753+b-bek6@users.noreply.github.com>
2024-01-28 22:29:53 +0545

    none

--- app/page.tsx
+++ app/page.tsx
@@ -7,43 +7,46 @@ import Footer from "./component/Footer"
 export default function page() {
     return (
         <>
-            <div className=" h-[100vh] max-h-screen  md:overflow-scroll overflow-hidden max-w-screen text-black bg-slate-200 ">
+            <div className="   md:overflow-scroll overflow-hidden max-w-screen text-black bg-slate-200 ">
 
-                <div className=" h-[80vh] grid  justify-center text-center items-center">
-                    <div className="m-10 z-50">
+
+               
+
+                <div className="  grid h-screen grid-rows-3 justify-center text-center items-center">
                     <div>
-                        <h2 className=" text-xl mt-3 ">Code Wave Tech</h2>
+                        <h2 className=" text-xl  ">Code Wave Tech</h2>
                     </div>
-                    <div className="  grid gap-4 align-middle justify-center text-center  mt-16" >
-                        <h1 className="text-4xl font-bold "> We are coming soon</h1>
+                    <div>
+
+                    <div className="  grid gap-4 align-middle justify-center text-center" >
+                        <h1 className=" z-50 text-4xl font-bold "> We are coming soon</h1>
                         {/* <p className="text-sm max-w-[355px] sm:max-w-auto text-wrap">Lorem ipsum dolor sit amet consectetur adipisicing elit  sed eiu sit amet consectetur adipisicing</p> */}
                     </div>
                     {/* <div>
                         <Dates />
                         <div className=" hover:bg-primary transition ease-in-out hover:scale-150 text-primary flex justify-center items-center p-4 px-8 my-20 rounded-full font-bold"> This Site is Under Construction </div>
                     </div> */}
-                    <div className="mt-10 grid gap-4">
-                        <p className="text-fourth text-sm font-bold">Want to be the first to know when we launch?</p>
-                    <div className=" font-bold grid gap-4 justify-center grid-flow-col" >
-                        <Notify/>
-                        {/* <button className="bg-white border w-fit text-secondary py-2 px-7 rounded-full ">More Info </button> */}
-                        {/* <About/> */}
+                    <div className="mt-5 grid gap-4">
+                        <p className=" text-fourth text-sm font-bold">Want to be the first to know when we launch?</p>
+                        <div className=" z-50  font-bold grid gap-4 justify-center grid-flow-col" >
+                            <Notify />
+                            {/* <button className="bg-white border w-fit text-secondary py-2 px-7 rounded-full ">More Info </button> */}
+                            {/* <About/> */}
+                        </div>
                     </div>
                     </div>
-                    <div className="absolute bottom-0 left-0 right-0 mb-2">
-                    <div>
-                        <SocialButtons/>
-                    </div>
-                    <div className="mt-5">
-                        <Footer/>
-                    </div>
-
-                    </div>
-
+                    <div className="z-50 ">
+                        <div>
+                            <SocialButtons />
+                        </div>
+                        <div className="mt-5">
+                            <Footer />
+                        </div>
                     </div>
 
-                    <div className="absolute opacity-80 w-screen overflow-hidden justify-end left-0 bottom-0 align-baseline items-end">
-                        <svg className="" xmlns="http://www.w3.org/2000/svg" version="1.1" width="1440" height="1080" preserveAspectRatio="none" viewBox="0 0 1440 1080">
+                    </div>
+                    <div className=" opacity-80  absolute bottom-0 w-screen overflow-hidden ">
+                        <svg  xmlns="http://www.w3.org/2000/svg" version="1.1" width="1440" height="1080" preserveAspectRatio="none" viewBox="0 0 1440 1080">
                             <g mask="url(&quot;#SvgjsMask1000&quot;)" fill="none">
                                 <path d="M1488 1080L0 1080 L0 828.87Q17.63 726.5, 120 744.14Q163.08 667.22, 240 710.3Q330.17 680.47, 360 770.64Q442.97 733.61, 480 816.57Q506.19 770.76, 552 796.94Q577.41 750.36, 624 775.77Q642.85 722.62, 696 741.46Q779.38 704.84, 816 788.22Q888.83 741.05, 936 813.88Q970.49 728.37, 1056 762.86Q1109.59 696.44, 1176 750.03Q1276.44 730.47, 1296 830.91Q1304.55 767.46, 1368 776.02Q1417.42 705.44, 1488 754.87z" fill="rgba(226, 62, 87, 1)"></path>
                                 <path d="M1464 1080L0 1080 L0 854.1Q61.37 795.48, 120 856.85Q177.75 842.6, 192 900.35Q221.24 857.59, 264 886.82Q288.42 791.24, 384 815.66Q437.18 748.84, 504 802.02Q555.55 781.57, 576 833.11Q669.63 806.74, 696 900.37Q725.88 858.25, 768 888.12Q821.34 869.46, 840 922.8Q853.63 864.43, 912 878.06Q910.84 804.9, 984 803.74Q1077.75 825.5, 1056 919.25Q1044.22 835.47, 1128 823.69Q1180.6 756.29, 1248 808.89Q1317.3 806.19, 1320 875.5Q1379.5 863, 1392 922.5Q1413.5 872, 1464 893.5z" fill="rgba(136, 48, 78, 1)"></path>
@@ -52,7 +55,6 @@ export default function page() {
                             </g>
                         </svg>
                     </div>
-                </div>
             </div>
 
         </>
