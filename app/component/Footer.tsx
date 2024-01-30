8aab2c2e0b636550cc456a5f8595146dd4766c15
Bibek Ghimire <89078753+b-bek6@users.noreply.github.com>
2024-01-30 13:19:43 +0545

    refactor

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { MdOutlineEmail } from "react-icons/md";
+import Link from 'next/link';
+import { FaLocationArrow, FaPhone } from 'react-icons/fa6';
 
 export default function Footer() {
   return (
-    <div className='flex justify-center align-middle items-center text-center text-white'>
-        Copyright By Code Wave Tech. All Rights Reserved
-    </div>
+    <>
+      <div className='grid md:grid-flow-col  grid-flow-row md:grid-cols-3 gap-4 justify-center text-white'>
+        <div className='flex gap-2 md:justify-center items-center'>
+          <Link href={`mailto:info@codewave.com.np?subject=Subscribe&body=Write us a message`}><div className='btns flex justify-center items-center w-10 h-10 bg-fourth text-primary'><MdOutlineEmail /></div></Link>
+          <label>info@codewave.com.np</label>
+        </div>
+        <div className='flex gap-2 md:justify-center items-center'>
+          <Link href={`geo:27.7172,85.3240`}> <div className='btns flex justify-center items-center w-10 h-10 bg-fourth text-primary'><FaLocationArrow /></div></Link>
+          <label>Kathmandu, Nepal</label>
+        </div>
+        <div className='flex gap-2 md:justify-center items-center'>
+          <Link href={`tel:9851008523`}> <div className='btns flex justify-center items-center w-10 h-10 bg-fourth text-primary'><FaPhone /></div></Link>
+          <label>9851008523</label>
+        </div>
+      </div>
+
+    </>
   )
 }
