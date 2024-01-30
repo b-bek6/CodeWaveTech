69847d1131706022dbfd7a8f0049fe86914faea1
Bibek Ghimire <89078753+b-bek6@users.noreply.github.com>
2024-01-30 13:24:46 +0545

    refactor2

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -8,15 +8,15 @@ export default function Footer() {
     <>
       <div className='grid md:grid-flow-col  grid-flow-row md:grid-cols-3 gap-4 justify-center text-white'>
         <div className='flex gap-2 md:justify-center items-center'>
-          <Link href={`mailto:info@codewave.com.np?subject=Subscribe&body=Write us a message`}><div className='btns flex justify-center items-center w-10 h-10 bg-fourth text-primary'><MdOutlineEmail /></div></Link>
+          <Link href={`mailto:info@codewave.com.np?subject=Subscribe&body=Write us a message`}><div className='flex justify-center items-center w-10 h-10  bg-white rounded-full text-primary'><MdOutlineEmail /></div></Link>
           <label>info@codewave.com.np</label>
         </div>
         <div className='flex gap-2 md:justify-center items-center'>
-          <Link href={`geo:27.7172,85.3240`}> <div className='btns flex justify-center items-center w-10 h-10 bg-fourth text-primary'><FaLocationArrow /></div></Link>
+          <Link href={`geo:27.7172,85.3240`}> <div className=' flex justify-center items-center w-10 h-10  bg-white rounded-full text-primary'><FaLocationArrow /></div></Link>
           <label>Kathmandu, Nepal</label>
         </div>
         <div className='flex gap-2 md:justify-center items-center'>
-          <Link href={`tel:9851008523`}> <div className='btns flex justify-center items-center w-10 h-10 bg-fourth text-primary'><FaPhone /></div></Link>
+          <Link href={`tel:9851008523`}> <div className=' flex justify-center items-center w-10 h-10 bg-white rounded-full text-primary'><FaPhone /></div></Link>
           <label>9851008523</label>
         </div>
       </div>
